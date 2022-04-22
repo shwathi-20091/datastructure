dd7032898fac139f9482989e115b9afce808b131
shwathi-20091 <shwathi.t@tringapps.com>
2022-04-22 11:25:05 +0530

    first

--- src/main/java/datastructures/QueueOperation.java
+++ src/main/java/datastructures/QueueOperation.java
@@ -1,11 +1,13 @@
 package datastructures;
 import java.util.*;
-public class queue {
+public class QueueOperation {
 	public static void main(String[] args)
 	{
         Scanner sc=new Scanner(System.in);
-		Queue<Integer> pq = new PriorityQueue<>();
-        int option,value,choice=1;
+		Queue<Integer> pq = new PriorityQueue<Integer>();
+        int option;
+        int value;
+        int choice=1;
         do{
             System.out.println("=========================================================================================");
         System.out.println("enter your option\n1.add an element\n2.delete an element\n3.top element\n4.print queue");
